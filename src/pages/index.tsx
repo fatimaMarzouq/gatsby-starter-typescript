83353eb3f3d3ef6e401a94f5821c339ff3fee8fc
fatima <fatima@softylus.com>
2021-10-17 12:36:41 +0300

    console log user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,20 @@ import IndexLayout from '../layouts'
 import { logout, getProfile } from "../utils/auth"
 const IndexPage = () => {
 
-  const user = getProfile()
+  const user = getProfile();
+  console.log(user);
   return (
 
     <IndexLayout>
       <Page>
         <Container>
 
-          <div>Hi, {user.name ? user.name : <><p>friend, You should <Link to='signin'>signin</Link> to see restricted content!</p><p>Do not have account <a href="https://dev--i4eym-u.us.auth0.com/u/signup?state=hKFo2SBUUU9Sc3p3WmRBZ04wTFhkOTNhaldIZ2dzTzVaZGtQZ6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIE1wNUpqc0pjN2UwRFZMT0RVenJGNzBubVZDTV9HbG1ko2NpZNkgQzR6S3FjZEdQemMyZEpvcUFUR1dWQUpNWDN0VjVOeEs">sign up</a></p></>}</div>
+          <h1>Hi, {user.name ? user.name : <>friend, You should <Link to='signin'>signin</Link> to see restricted content! Do not have account <a href="https://dev--i4eym-u.us.auth0.com/u/signup?state=hKFo2SBUUU9Sc3p3WmRBZ04wTFhkOTNhaldIZ2dzTzVaZGtQZ6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIE1wNUpqc0pjN2UwRFZMT0RVenJGNzBubVZDTV9HbG1ko2NpZNkgQzR6S3FjZEdQemMyZEpvcUFUR1dWQUpNWDN0VjVOeEs">sign up</a></>}</h1>
           {user.name ?
             <>
               You are logged in, so check your{" "}
               <Link to="regularFetures">regular Fetures</Link>
+              <br />
               <Link
                 to="/logout"
                 onClick={e => {
