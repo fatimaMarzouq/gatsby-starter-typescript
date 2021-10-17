6c28e51995101f917c4f0228667c01eebe9c8753
fatima <fatima@softylus.com>
2021-10-17 12:48:47 +0300

    roles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ const IndexPage = () => {
       <Page>
         <Container>
 
-          <h1>Hi, {user.name ? user.name : <>friend, You should <Link to='signin'>signin</Link> to see restricted content! Do not have account <a href="https://dev--i4eym-u.us.auth0.com/u/signup?state=hKFo2SBUUU9Sc3p3WmRBZ04wTFhkOTNhaldIZ2dzTzVaZGtQZ6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIE1wNUpqc0pjN2UwRFZMT0RVenJGNzBubVZDTV9HbG1ko2NpZNkgQzR6S3FjZEdQemMyZEpvcUFUR1dWQUpNWDN0VjVOeEs">sign up</a></>}</h1>
+          <h1>Hi {user.name ? user.name : <>friend, You should <Link to='signin'>signin</Link> to see restricted content! Do not have account? <a href="https://dev--i4eym-u.us.auth0.com/u/signup?state=hKFo2SBUUU9Sc3p3WmRBZ04wTFhkOTNhaldIZ2dzTzVaZGtQZ6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIE1wNUpqc0pjN2UwRFZMT0RVenJGNzBubVZDTV9HbG1ko2NpZNkgQzR6S3FjZEdQemMyZEpvcUFUR1dWQUpNWDN0VjVOeEs">sign up</a></>}</h1>
           {user.name ?
             <>
               You are logged in, so check your{" "}
