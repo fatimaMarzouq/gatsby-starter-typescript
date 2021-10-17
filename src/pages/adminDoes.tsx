8d9d4745ad8cde690e04608605ed49b8303c414e
fatima <fatima@softylus.com>
2021-10-17 10:40:06 +0300

    no roles

--- src/pages/adminDoes.tsx
+++ src/pages/adminDoes.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react'
 import { Link, navigate } from 'gatsby'
-import { getUser, isLoggedIn, logout } from "../utils/auth"
+// import { getUser, isLoggedIn, logout } from "../utils/auth"
+import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
 
-const adminDoes = () => (
-  <IndexLayout>
-    <Page>
-      <Container>
-        <h1>Hi from admin Does page</h1>
-        <p>Welcome to admin Does</p>
-        <ul>
-          <li>
-            <Link to="/adminFetures/">admin Fetures</Link>
-          </li>
-          <li>
-            {isLoggedIn() ? (
+const adminDoes = () => {
+  const user = getProfile()
+  return (
+    <IndexLayout>
+      <Page>
+        <Container>
+          <h1>Hi from admin Does page</h1>
+          <p>Welcome to admin Does</p>
+          <ul>
+            <li>
+              <Link to="/adminFetures/">admin Fetures</Link>
+            </li>
+            <li>
+              {/* {isLoggedIn() ? (
               <Link
                 to="/logout"
                 onClick={event => {
@@ -26,12 +29,26 @@ const adminDoes = () => (
               >
                 Logout
               </Link>
-            ) : null}
-          </li>
-        </ul>
-      </Container>
-    </Page>
-  </IndexLayout>
-)
+            ) : null} */}
+              <>
+                <p>Hi, {user.name ? user.name : "friend"}!</p>
+                <Link
+                  to="/logout"
+                  onClick={e => {
+                    logout()
+                    e.preventDefault()
+                  }}
+                >
+                  Log Out
+                </Link>
+              </>
+
+            </li>
+          </ul>
+        </Container>
+      </Page>
+    </IndexLayout>
+  )
+}
 
 export default adminDoes
